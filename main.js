aa6ce9f431514f79ec749ee61ea8097badef01af
DelaramRajaei <rajaiedelaram@gmail.com>
2021-12-02 02:14:05 +0330

    css updated

--- main.js
+++ main.js
@@ -6,10 +6,18 @@ inquery = (e) => {
     .then((response) => response.json())
     .then((result) => {
       console.log(result);
-      document.getElementById(result_gender).innerText = result.gender;
-      document.getElementById(result_probability).innerText = result.probability;
+      document.getElementById(
+        "result_gender"
+      ).innerText = capitalizeFirstLetter(result.gender);
+      document.getElementById(
+        "result_probability"
+      ).innerText = capitalizeFirstLetter(result.probability);
     })
     .catch((err) => console.error(err));
+};
+
+capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
 const form = document.getElementById("application_form");
